e61d3881ca989403298dd62b4d3e75f35c5a8828
jfyh1234 <2744260460@qq.com>
2023-05-24 16:15:19 +0800

    init

---
+++ src/main/java/com/read/read_book/ReadBookApplication.java
@@ -0,0 +1,13 @@
+package com.read.read_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/read/read_book/ReadBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.read.read_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
