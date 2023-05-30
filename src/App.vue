f538742bc8c1eb47176ca22cf824d4f1c3f4a1db
songzhiyong <songzhiyong97@gmail.com>
2023-05-30 15:37:23 +0800

    commit register and login page
    First time commit the register and login page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<!--  <div>
+    <nav>
+      <router-link to="/">HelloWorld</router-link>
+      <router-link to="/login">login</router-link>
+    </nav>-->
+<!--  <h1>hello</h1>-->
+    <router-view/>
+<!--  </div>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+// import HelloWorld from './components/HelloWorld.vue'
+// import WebLogin from "@/views/WebLogin.vue";
+/*export default {
   name: 'App',
   components: {
-    HelloWorld
+    WebLogin
   }
-}
+}*/
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
