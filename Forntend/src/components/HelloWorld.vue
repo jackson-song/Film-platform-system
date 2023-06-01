41c640c614b6e0bee385fb7c185e64cac2c1ba64
songzhiyong <songzhiyong97@gmail.com>
2023-06-01 16:57:20 +0800

    更新Frontend目录

---
+++ Forntend/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>helloworld</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
