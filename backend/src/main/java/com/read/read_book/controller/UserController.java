3f2645500aa37e3fc10e6cd7a2f587eec6d0ef41
wangbin <3107878610@qq.com>
2023-06-01 16:19:58 +0800

    文件结构调整

--- backend/src/main/java/com/read/read_book/controller/UserController.java
+++ backend/src/main/java/com/read/read_book/controller/UserController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class UserController {
 
-    @RequestMapping("/index/")
-    public String index() {
-        return "/index.html";
+//    注意访问路径最后的/必须要
+    @RequestMapping("/user/")
+    public String user() {
+        return "/hello.html";
+    }
+
+    @RequestMapping("/user2/")
+    public String user2() {
+        return "hello user2";
     }
 }
